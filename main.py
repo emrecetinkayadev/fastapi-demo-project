55ee202967072e7876b3e4c72e950a7e48e6fdca
emrecetinkayadev <emrecetinkayadev@gmail.com>
2023-01-25 11:18:00 +0300

    added GET:random-word async version.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import aiohttp
 
 app = FastAPI()
 
@@ -6,3 +7,14 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"hello": "World"}
+
+
+@app.get("/random-word")
+async def random_word():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://random-word-api.herokuapp.com/word") as resp:
+            data = await resp.json()
+            if data:
+                return data
+            else:
+                return None
